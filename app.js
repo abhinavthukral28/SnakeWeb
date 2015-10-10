edeeb8ffc4ac0af7fa93dfba1c65ecdb65b32111
allanjluke <allanjluke@gmail.com>
2015-10-10 02:25:39 -0400

    the things

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var app = require('http').createServer(handler)
+var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(process.env.PORT,process.env.IP);
+app.listen(3000);
 
 function handler (req, res) {
-  fs.readFile(__dirname + 'html/index.html',
+  fs.readFile(__dirname + '/html/index.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -15,7 +15,7 @@ function handler (req, res) {
     res.writeHead(200);
     res.end(data);
   });
-console.log("hi handler");
+ ;
 }
 
 io.on('connection', function (socket) {
