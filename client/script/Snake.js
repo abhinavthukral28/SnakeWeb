4265d185e0ed57dee280864358e97ceb24e84495
Abhinav Thukral <abhinav.thukral@carleton.ca>
2015-10-17 19:08:05 +0000

    multiple turns, still a bug if you change directions quickly

--- client/script/Snake.js
+++ client/script/Snake.js
@@ -1,91 +1,92 @@
  var Snake = function() {
-     var thisObj = this;
-     var DIRECTION = Snake.directions.UP;
-     var color = Snake.colors.BLUE;
-     var length;
-     var pivot = undefined;
-     
-     var pivotPoints = [];
-
-     var nodes = [];
-
-   
-     
-     //implement
-     //this.removeNode(node)
-     //this.setDirection(node)
-     //this.getHead()
-     //this
-
-     this.getNodes = function() {
-         return nodes;
-     }
-
-     this.addNode = function() {
-         var lastNode = nodes[nodes.length - 1];
-         var newNode = new Node (0,0,DIRECTION);
-         switch (lastNode.direction) {
-             case Snake.directions.DOWN:
-                 newNode.x = lastNode.x;
-                 newNode.y = lastNode.y-Node.radius;
-                 break;
-             case Snake.directions.LEFT:
-                 newNode.x =lastNode.x+Node.radius;
-                 newNode.y = lastNode.y;
-                 break;
-             case Snake.directions.RIGHT:
-                 newNode.x = lastNode.x-Node.radius;
-                 newNode.y = lastNode.y;
-                 break;
-             case Snake.directions.UP:
-                 newNode.x = lastNode.x;
-                 newNode.y = lastNode.y+Node.radius;
-                 break;
-             default:
-                 break;
+  var thisObj = this;
+  var DIRECTION = Snake.directions.UP;
+  var color = Snake.colors.BLUE;
+  var length;
+  var nodes = [];
 
 
-         }
-         nodes.push(newNode);
-     }
+
+  //implement
+  //this.removeNode(node)
+  //this.setDirection(node)
+  //this.getHead()
+  //this
+
+  this.getNodes = function() {
+   return nodes;
+  }
+
+  this.addNode = function() {
+   var lastNode = nodes[nodes.length - 1];
+   var newNode = new Node(0, 0, DIRECTION);
+   switch (lastNode.direction) {
+    case Snake.directions.DOWN:
+     newNode.x = lastNode.x;
+     newNode.y = lastNode.y - Node.radius;
+     break;
+    case Snake.directions.LEFT:
+     newNode.x = lastNode.x + Node.radius;
+     newNode.y = lastNode.y;
+     break;
+    case Snake.directions.RIGHT:
+     newNode.x = lastNode.x - Node.radius;
+     newNode.y = lastNode.y;
+     break;
+    case Snake.directions.UP:
+     newNode.x = lastNode.x;
+     newNode.y = lastNode.y + Node.radius;
+     break;
+    default:
+     break;
 
 
-     this.getDirection = function() {
-         return DIRECTION;
-     }
+   }
+   nodes.push(newNode);
+  }
 
-     this.getColor = function() {
-         return color;
-     }
 
-     this.startTurn = function(direction) {
-            pivotPoints
-         if (!thisObj.isTurning()) {
+  this.getDirection = function() {
+   return DIRECTION;
+  }
 
-             pivot = [];
-             pivot[0] = nodes[0].x;
-             pivot[1] = nodes[0].y;
+  this.getColor = function() {
+   return color;
+  }
 
-             nodes[0].direction = direction;
-             DIRECTION = direction;
-         }
-     }
+  this.startTurn = function(direction) {
 
-     this.getPivot = function() {
-         return pivot;
-     }
 
-     this.isTurning = function() {
-         return pivot !== undefined;
-     }
+   var pivot = [];
+   pivot[0] = nodes[0].x;
+   pivot[1] = nodes[0].y;
+   for (var i = 0; i < nodes.length; i++) {
+      nodes[i].pivots.push(pivot);
+   }
 
-     this.stopTurn = function() {
-         pivot = undefined;
-     }
-     
-      nodes.push(new Node(50, 50, Snake.directions.UP));
-      for (var i = 0; i < 50; i++)
-        this.addNode();
+   // pivotPoints.push(pivot);
+   // nodes[0].direction = direction;
+   DIRECTION = direction;
+
+  }
+
+  this.getPivot = function() {
+   return pivot;
+  }
+
+  this.isTurning = function() {
+   return pivot !== undefined;
+  }
+
+  this.stopTurn = function() {
+   pivot = undefined;
+   return pivotPoints.shift();
+  }
+
+  //NEED TO INIT FROM PURELY ADD NODE
+  nodes.push(new Node(50, 50, Snake.directions.UP));
+  for (var i = 0; i < 5; i++)
+   this.addNode();
 
  }
 
@@ -93,29 +94,31 @@
 
 
  Snake.fromJSON = function(jsonString) {
-     //parseJSONString
+  //parseJSONString
  }
  Snake.colors = {
-     RED: "red",
-     BLUE: "blue",
-     YELLOW: "yellow",
-     WHITE: "white",
-     ORANGE: "orange"
+  RED: "red",
+  BLUE: "blue",
+  YELLOW: "yellow",
+  WHITE: "white",
+  ORANGE: "orange"
  }
 
  Snake.directions = {
-     UP: "UP",
-     DOWN: "DOWN",
-     LEFT: "LEFT",
-     RIGHT: "RIGHT"
+  UP: "UP",
+  DOWN: "DOWN",
+  LEFT: "LEFT",
+  RIGHT: "RIGHT"
  };
 
 
 
  var Node = function(x, y, direction) {
-     this.direction = direction;
-     this.x = x;
-     this.y = y;
+  this.direction = direction;
+  this.color = undefined;
+  this.pivots = []
+  this.x = x;
+  this.y = y;
  }
 
 
--- client/script/game.js
+++ client/script/game.js
@@ -1,9 +1,9 @@
-var canvas = document.getElementById("gameCanvas");
+var canvas;
 var userSnake;
 var snakes = [];
 var timer;
 $(document).ready(function() {
-    
+
     initHandlers();
     var snake = new Snake();
     snakes.push(snake);
@@ -18,67 +18,68 @@ $(document).ready(function() {
 function drawCanvas() {
     var context = canvas.getContext('2d');
     context.fillStyle = 'black';
-    context.fillRect(0,0,canvas.width,canvas.height);
-     
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
     var snake;
     var nodes;
     var tempNode;
     for (var i = 0; i < snakes.length; i++) {
         snake = snakes[i];
-        nodes = snake.getNodes()
+        nodes = snake.getNodes();
         for (var j = 0; j < nodes.length; j++) {
-            
+
             tempNode = nodes[j];
-            
+
             context.beginPath();
             context.arc(tempNode.x, tempNode.y, Node.radius, 0, 2 * Math.PI, false);
             context.fillStyle = snake.getColor();
             context.fill();
             context.stroke();
-   
-            switch (tempNode.direction)
-            {
-                case Snake.directions.DOWN:
-                      tempNode.y++;
-                      break;
-                case Snake.directions.LEFT:
-                     tempNode.x--;
-                     break;
-                case Snake.directions.RIGHT:
-                     tempNode.x++;
-                     break;
-                case Snake.directions.UP:
-                     tempNode.y--;
-                     break;
-                default:
-                    break;
-                    
-                
-            }
-            
-                     
-            if (snake.isTurning())
-            {
-                var pivot = snake.getPivot();
-                
-                if (tempNode.x === pivot[0] && tempNode.y === pivot[1])
-                {
-                    if (tempNode.direction != snake.getDirection())
+
+            if (tempNode.pivots.length !== 0) {
+                var pivot = tempNode.pivots[0];
+
+                if (tempNode.x === pivot[0] && tempNode.y === pivot[1]) {
+                    if (j == 0) {
                         tempNode.direction = snake.getDirection();
-                    if (j+1 == nodes.length)
-                    {
-                        snake.stopTurn();
                     }
+                    else {
+                        tempNode.direction = nodes[j - 1].direction;
+                    }
+                    tempNode.pivots.shift();
+               
+
                 }
             }
-            
-            
-             
+            switch (tempNode.direction) {
+                case Snake.directions.DOWN:
+                    tempNode.y++;
+                    break;
+                case Snake.directions.LEFT:
+                    tempNode.x--;
+                    break;
+                case Snake.directions.RIGHT:
+                    tempNode.x++;
+                    break;
+                case Snake.directions.UP:
+                    tempNode.y--;
+                    break;
+                default:
+                    break;
+
+
+            }
+
+
+
+
+
+
         }
 
 
-    } 
-   
+    }
+
 
 
 
@@ -89,52 +90,50 @@ function handleTimer() {
     drawCanvas();
 }
 
-function initHandlers(){
+function initHandlers() {
     $(document).keydown(handleKeyDown);
 }
 
 
-	var ENTER = 13;
-	var RIGHT_ARROW = 39;
-	var LEFT_ARROW = 37;
-	var UP_ARROW = 38;
-	var DOWN_ARROW = 40;
+var ENTER = 13;
+var RIGHT_ARROW = 39;
+var LEFT_ARROW = 37;
+var UP_ARROW = 38;
+var DOWN_ARROW = 40;
 
 
-function handleKeyDown(e){
-	var keyPress = e.which;
-	var currentDirection = userSnake.getDirection();
-	switch (keyPress) {
-	    case UP_ARROW:
-	        if (currentDirection != Snake.directions.DOWN)
-	            userSnake.startTurn(Snake.directions.UP);
-	        e.stopPropagation();
-        	e.preventDefault();
-	        break;
-	    case RIGHT_ARROW:
-	        if (currentDirection != Snake.directions.LEFT)
-	            userSnake.startTurn(Snake.directions.RIGHT);
-	        e.stopPropagation();
-         	e.preventDefault();
-	        break;
-	    case LEFT_ARROW:
-	        if (currentDirection != Snake.directions.RIGHT)
-	            userSnake.startTurn(Snake.directions.LEFT);
-	        e.stopPropagation();
-	        e.preventDefault();
-	        break;
-	    case DOWN_ARROW:
-	        if (currentDirection != Snake.directions.UP)
-	            userSnake.startTurn(Snake.directions.DOWN);
-	        e.stopPropagation();
-        	e.preventDefault();
-	        break;
-	    default:
-	        break;
-	}
-	
- 
+function handleKeyDown(e) {
+    var keyPress = e.which;
+    var currentDirection = userSnake.getDirection();
+    switch (keyPress) {
+        case UP_ARROW:
+            if (currentDirection != Snake.directions.DOWN)
+                userSnake.startTurn(Snake.directions.UP);
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        case RIGHT_ARROW:
+            if (currentDirection != Snake.directions.LEFT)
+                userSnake.startTurn(Snake.directions.RIGHT);
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        case LEFT_ARROW:
+            if (currentDirection != Snake.directions.RIGHT)
+                userSnake.startTurn(Snake.directions.LEFT);
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        case DOWN_ARROW:
+            if (currentDirection != Snake.directions.UP)
+                userSnake.startTurn(Snake.directions.DOWN);
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        default:
+            break;
+    }
+
+
 
 }
-
- 
